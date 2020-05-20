5e07f4de554b1313279c345c43fa50e556a2f4db
ross <ross.pure@gmail.com>
2020-05-20 20:00:30 +1000

    add more complete consensus logic

--- brng/mock/consensus.go
+++ brng/mock/consensus.go
@@ -7,31 +7,59 @@ import (
 )
 
 type PullConsensus struct {
-	done        bool
-	leaderIndex secp256k1.Secp256k1N
-	threshold   int
-	table       brng.Table
-	checker     shamir.VSSChecker
+	done          bool
+	honestIndices []secp256k1.Secp256k1N
+	threshold     int
+	table         brng.Table
+	checker       shamir.VSSChecker
 }
 
 func (pc *PullConsensus) HandleRow(row brng.Row) bool {
+	if pc.done {
+		return true
+	}
+
+	for _, sharing := range row {
+		for _, index := range pc.honestIndices {
+			// Only check the share corresponding to the leader index; in
+			// practice the shares will be encrypted for each party and so the
+			// leader can only check their own indices.
+			share, err := sharing.ShareWithIndex(index)
+			if err != nil {
+				panic("row should contain all honest indices")
+			}
+
+			c := sharing.Commitment()
+			if !pc.checker.IsValid(&c, &share) {
+				return pc.done
+			}
+		}
+	}
+
+	pc.table = append(pc.table, row)
+	if len(pc.table) == pc.threshold {
+		pc.done = true
+	}
+
 	return pc.done
 }
 
 func (pc PullConsensus) TableIsValid() bool {
 	for _, row := range pc.table {
 		for _, sharing := range row {
-			// Only check the share corresponding to the leader index; in
-			// practice the shares will be encrypted for each party and so the
-			// leader can only check their own indices.
-			share, err := sharing.ShareWithIndex(pc.leaderIndex)
-			if err != nil {
-				panic("row should contain the leader index")
-			}
+			for _, index := range pc.honestIndices {
+				// Only check the share corresponding to the leader index; in
+				// practice the shares will be encrypted for each party and so the
+				// leader can only check their own indices.
+				share, err := sharing.ShareWithIndex(index)
+				if err != nil {
+					panic("row should contain all honest indices")
+				}
 
-			c := sharing.Commitment()
-			if !pc.checker.IsValid(&c, &share) {
-				return false
+				c := sharing.Commitment()
+				if !pc.checker.IsValid(&c, &share) {
+					return false
+				}
 			}
 		}
 	}
