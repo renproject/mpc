aaf2a4b37050e7176f1f1e58c0c60db48ec8ea28
ross <ross.pure@gmail.com>
2020-06-24 13:24:06 +1000

    test rng state

---
+++ rng/state_test.go
@@ -0,0 +1,57 @@
+package rng_test
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/renproject/mpc/rng"
+)
+
+var _ = Describe("State", func() {
+	rand.Seed(int64(time.Now().Nanosecond()))
+
+	Context("String", func() {
+		Specify("the Stringer interface should be properly implemented", func() {
+			var expected string
+			for i := 0; i < 256; i++ {
+				switch i {
+				case 0:
+					expected = "Init"
+				case 1:
+					expected = "WaitingOpen"
+				case 2:
+					expected = "Done"
+				default:
+					expected = fmt.Sprintf("Unknown(%v)", uint8(i))
+				}
+
+				Expect(rng.State(i).String()).To(Equal(expected))
+			}
+		})
+	})
+
+	Context("Marshalling", func() {
+		It("should be equal after marshalling and unmarshalling", func() {
+			buf := bytes.NewBuffer([]byte{})
+			for i := 0; i < 256; i++ {
+				buf.Reset()
+				s1 := rng.State(i)
+				m, err := s1.Marshal(buf, s1.SizeHint())
+				Expect(err).ToNot(HaveOccurred())
+				Expect(m).To(Equal(0))
+
+				var s2 rng.State
+				m, err = s2.Unmarshal(buf, s2.SizeHint())
+				Expect(err).ToNot(HaveOccurred())
+				Expect(m).To(Equal(0))
+
+				Expect(s1).To(Equal(s2))
+			}
+		})
+	})
+})
