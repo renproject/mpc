cb6de0caaa6a3ced79754fa8686ad6c3433c7cb9
ross <ross.pure@gmail.com>
2020-07-27 11:18:49 +1000

    loop over all types for matshalling tests

--- open/marshal_test.go
+++ open/marshal_test.go
@@ -13,47 +13,53 @@ import (
 
 var _ = Describe("Surge marshalling", func() {
 	trials := 10
-	t := reflect.TypeOf(open.Opener{})
+	tys := []reflect.Type{
+		reflect.TypeOf(open.State{}),
+		reflect.TypeOf(open.Opener{}),
+	}
 
-	Context(fmt.Sprintf("surge marshalling and unmarshalling for %v", t), func() {
-		It("should be the same after marshalling and unmarshalling", func() {
-			for i := 0; i < trials; i++ {
-				Expect(surgeutil.MarshalUnmarshalCheck(t)).To(Succeed())
-			}
-		})
-
-		It("should not panic when fuzzing", func() {
-			for i := 0; i < trials; i++ {
-				Expect(func() { surgeutil.Fuzz(t) }).ToNot(Panic())
-			}
-		})
-
-		Context("marshalling", func() {
-			It("should return an error when the buffer is too small", func() {
+	for _, t := range tys {
+		t := t
+		Context(fmt.Sprintf("surge marshalling and unmarshalling for %v", t), func() {
+			It("should be the same after marshalling and unmarshalling", func() {
 				for i := 0; i < trials; i++ {
-					Expect(surgeutil.MarshalBufTooSmall(t)).To(Succeed())
+					Expect(surgeutil.MarshalUnmarshalCheck(t)).To(Succeed())
 				}
 			})
 
-			It("should return an error when the memory quota is too small", func() {
+			It("should not panic when fuzzing", func() {
 				for i := 0; i < trials; i++ {
-					Expect(surgeutil.MarshalRemTooSmall(t)).To(Succeed())
-				}
-			})
-		})
-
-		Context("unmarshalling", func() {
-			It("should return an error when the buffer is too small", func() {
-				for i := 0; i < trials; i++ {
-					Expect(surgeutil.UnmarshalBufTooSmall(t)).To(Succeed())
+					Expect(func() { surgeutil.Fuzz(t) }).ToNot(Panic())
 				}
 			})
 
-			It("should return an error when the memory quota is too small", func() {
-				for i := 0; i < trials; i++ {
-					Expect(surgeutil.UnmarshalRemTooSmall(t)).To(Succeed())
-				}
+			Context("marshalling", func() {
+				It("should return an error when the buffer is too small", func() {
+					for i := 0; i < trials; i++ {
+						Expect(surgeutil.MarshalBufTooSmall(t)).To(Succeed())
+					}
+				})
+
+				It("should return an error when the memory quota is too small", func() {
+					for i := 0; i < trials; i++ {
+						Expect(surgeutil.MarshalRemTooSmall(t)).To(Succeed())
+					}
+				})
+			})
+
+			Context("unmarshalling", func() {
+				It("should return an error when the buffer is too small", func() {
+					for i := 0; i < trials; i++ {
+						Expect(surgeutil.UnmarshalBufTooSmall(t)).To(Succeed())
+					}
+				})
+
+				It("should return an error when the memory quota is too small", func() {
+					for i := 0; i < trials; i++ {
+						Expect(surgeutil.UnmarshalRemTooSmall(t)).To(Succeed())
+					}
+				})
 			})
 		})
-	})
+	}
 })
