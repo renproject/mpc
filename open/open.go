b3be349ecef2eeeb61b8c2c76abc15208e3c587b
ross <ross.pure@gmail.com>
2020-07-28 12:43:19 +1000

    update docs

--- open/open.go
+++ open/open.go
@@ -11,81 +11,20 @@ import (
 	"github.com/renproject/surge"
 )
 
-// Opener is a state machine that handles the collecting of verifiable secret
-// shares and the reconstruction (if possible) of these shares to yield the
-// underlying secret. The state machine is designed to be resettable: the same
-// instance of a Opener can be used to open many different sharings, as opposed
-// to being one time use. However, each instance of the state machine is
-// specific to a given set of indices of the parties (i.e. the set of possible
-// indices that shares can have) and system parameter for the Pedersen
-// commitments used in the verifiable secret sharing scheme.
+// Opener is a state machine that is responsible for opening the secret value
+// for a verifiable sharing. An instance of this state machine has a specific
+// commitment used for share verification, a specific value of the Pedersen
+// parameter (h), and a specific set of indices for the participating players.
 //
-// The state machine states and transitions are as follows. At a high level,
-// the states need to capture:
+// The description of the state machine is simple: the state is a buffer of
+// shares that have been validated, and each time a share is received, it is
+// checked against the commitment and other parameters, and if it is valid it
+// is added to the buffer. Once enough shares have been added to the buffer for
+// reconstruction, the reconstructed secret is returned.
 //
-//	- The current sharing instance
-//	- Whether or not there are enough valid shares to perform a reconstruction
-//
-// The information needed for the sharing instance is captured by the
-// commitment `c` for the verifiable secret sharing instance, and the
-// associated reconstruction threshold `k`. The information that determines
-// whether it is possible to reconstruct is simply the number of valid shares
-// received `i`, and also `k`. The number of shares received is maintained
-// indirectly by storing all valid shares received, the latter of course need
-// to be saved in order to be able to perform a reconstruction. These three
-// variables, `c`, `k` and `i`, represent the entirety of the state needed for
-// the state transition logic. It is however convenient to group subsets of the
-// possible states into higher level states that reflect the higher level
-// operation of the state machine. The two main higher level states are Waiting
-// and Done. Additionally, there is an additional state Uninitialised that the
-// state machine is in upon construction and once moving to one of the other
-// two states will never be in this state again. To summarise, the high level
-// states (parameterised by the relevant parts of the lower level state) are
-//
-//	1. Uninitialised
-//	2. Waiting(`c`, `k`, `i`)
-//	3. Done(`c`)
-//
-// Now we consider the state transitions. There are only two triggers for state
-// transitions:
-//
-//	- Receiving a new share
-//	- Resetting for a new sharing instance
-//
-// The Opener state machine functions in batches, and has a batch size
-// associated with it. The number of shares it receives every time MUST be
-// equal to the batch size. When it has received threshold number of sets
-// of shares, it reconstructs all the secrets, a total of batch size in number.
-//
-// All of the state transitions are listed in the following and are grouped by
-// the current state. The condition Valid('c') for a share means that the share
-// is valid with repect to the commitment `c` and also that the index for the
-// share does not equal any of the indices in the current set of valid shares
-// and that it is in the list of indices that the state machine was constructed
-// with.
-//
-//	- Uninitialised
-//		- New instance(`c`, `k`) 	-> Waiting(`c`, `k`, `0`)
-//		- Otherwise 			-> Do nothing
-//	- Waiting(`c`, `k`, `k-1`)
-//		- New instance(`c`, `k`) 	-> Waiting(`c`, `k`, `0`)
-//		- Share, Valid(`c`) 		-> Done(`c`)
-//		- Otherwise 			-> Do nothing
-//	- Waiting(`c`, `k`, `i`), `i` < `k-1`
-//		- New instance(`c`, `k`) 	-> Waiting(`c`, `k`, `0`)
-//		- Share, Valid(`c`) 		-> Waiting(`c`, `k`, `i+1`)
-//		- Otherwise 			-> Do nothing
-//	- Done(`c`)
-//		- New instance(`c`, `k`) 	-> Waiting(`c`, `k`, `0`)
-//		- Otherwise 			-> Do nothing
-//
-// Alternatively, the state transitions can be grouped by message.
-//
-//	- New instance(`c`, `k`)
-//		- Any -> Waiting(`c`, `k`, `0`)
-//	- Share, Valid(`c`)
-//		- Waiting(`c`, `k`, `k-1`) 		-> Done(`c`)
-//		- Waiting(`c`, `k`, `i`), `i` < `k-1` 	-> Waiting(`c`, `k`, `i+1`)
+// The state machine is set up to enable batching. If several secrets need to
+// be opened at once, instead of having multiple state machines, just the one
+// is used and the incoming shares are processed in batches.
 type Opener struct {
 	// State
 	shareBufs []shamir.VerifiableShares
@@ -114,8 +53,7 @@ func (opener Opener) I() int {
 }
 
 // New returns a new instance of the Opener state machine for the given set of
-// indices and the given Pedersen commitment system parameter. The state
-// machine begins in the Uninitialised state.
+// indices and the given Pedersen commitment system parameter.
 func New(commitmentBatch []shamir.Commitment, indices []secp256k1.Fn, h secp256k1.Point) Opener {
 	// The batch size must be at least 1.
 	b := uint32(len(commitmentBatch))
@@ -151,10 +89,13 @@ func New(commitmentBatch []shamir.Commitment, indices []secp256k1.Fn, h secp256k
 	}
 }
 
-// HandleShareBatch handles the state transition logic upon receiving a set of
-// shares, and returns a ShareEvent that describes the outcome of the state
-// transition.  See the documentation for the different ShareEvent possiblities
-// for their significance.
+// HandleShareBatch handles the state transition logic upon receiving a batch
+// of shares, and returns a ShareEvent that describes the outcome of the state
+// transition. See the documentation for the different ShareEvent possiblities
+// for their significance. If enough shares have been received to reconstruct
+// the secret, then this is returned, otherwise the corresponding return value
+// is nil. Similarly, the decommitment (or hiding) value for the verifiable
+// sharing will also be returned.
 func (opener *Opener) HandleShareBatch(shareBatch shamir.VerifiableShares) (
 	ShareEvent,
 	[]secp256k1.Fn,
