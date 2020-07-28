63061b6f003b662812d52a8b954eb95246d2f52f
ross <ross.pure@gmail.com>
2020-07-28 16:55:53 +1000

    fix docs

--- open/err.go
+++ open/err.go
@@ -5,18 +5,17 @@ import "errors"
 var (
 	// ErrDuplicateIndex signifies that the received share has an index that is
 	// the same as the index of one of the shares that is already in the list
-	// of valid shares received for the current sharing instance. This can be
-	// output in both the Waiting and Done states.
+	// of valid shares received for the current sharing instance.
 	ErrDuplicateIndex = errors.New("duplicate index")
 
 	// ErrIndexOutOfRange signifies that the received share has an index that
 	// is not in the set of indices that the state machine was constructed
-	// with.  This can be output in both the Waiting and Done states.
+	// with.
 	ErrIndexOutOfRange = errors.New("index out of range")
 
 	// ErrInvalidShares signifies that at least one out of the received shares
 	// is not valid with respect to the commitment for the current sharing
-	// instance. This can be output in both the Waiting and Done states.
+	// instance.
 	ErrInvalidShares = errors.New("invalid shares")
 
 	// ErrIncorrectBatchSize signifies that the batch size of the received
