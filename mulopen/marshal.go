5125caee6b43a54a8d3ca22b8a3c6bc06f0cc6e4
ross <ross.pure@gmail.com>
2020-08-05 14:41:34 +1000

    marshalling tests for mulopener and message

--- mulopen/marshal.go
+++ mulopen/marshal.go
@@ -1,6 +1,15 @@
 package mulopen
 
-import "github.com/renproject/surge"
+import (
+	"math/rand"
+	"reflect"
+
+	"github.com/renproject/mpc/mulopen/mulzkp"
+	"github.com/renproject/secp256k1"
+	"github.com/renproject/shamir"
+	"github.com/renproject/shamir/shamirutil"
+	"github.com/renproject/surge"
+)
 
 // SizeHint implements the surge.SizeHinter interface.
 func (mulopener MulOpener) SizeHint() int {
@@ -80,6 +89,53 @@ func (mulopener *MulOpener) Unmarshal(buf []byte, rem int) ([]byte, int, error) 
 	return mulopener.h.Unmarshal(buf, rem)
 }
 
+// Generate implements the quick.Generator interface.
+func (mulopener MulOpener) Generate(_ *rand.Rand, size int) reflect.Value {
+	size /= 5
+	n := rand.Intn(size/2) + 1
+	k := uint32(rand.Intn(size/2) + 2)
+	batchSize := uint32(size) / k
+	if batchSize == 0 {
+		batchSize++
+	}
+	shareBufs := make([]shamir.Shares, batchSize)
+	numReceived := rand.Intn(n)
+	for i := range shareBufs {
+		shareBufs[i] = shamir.Shares{}
+		for j := 0; j < numReceived; j++ {
+			shareBufs[i] = append(shareBufs[i],
+				shamir.Share{
+					Index: secp256k1.RandomFn(),
+					Value: secp256k1.RandomFn(),
+				},
+			)
+		}
+	}
+	aCommitmentBatch := make([]shamir.Commitment, batchSize)
+	bCommitmentBatch := make([]shamir.Commitment, batchSize)
+	rzgCommitmentBatch := make([]shamir.Commitment, batchSize)
+	for i := uint32(0); i < batchSize; i++ {
+		for j := uint32(0); j < k; j++ {
+			aCommitmentBatch[i].Append(secp256k1.RandomPoint())
+			bCommitmentBatch[i].Append(secp256k1.RandomPoint())
+			rzgCommitmentBatch[i].Append(secp256k1.RandomPoint())
+		}
+	}
+	indices := shamirutil.RandomIndices(n)
+	h := secp256k1.RandomPoint()
+	mo := MulOpener{
+		shareBufs,
+		batchSize,
+		k,
+		aCommitmentBatch,
+		bCommitmentBatch,
+		rzgCommitmentBatch,
+		indices,
+		h,
+	}
+	return reflect.ValueOf(mo)
+}
+
 // SizeHint implements the surge.SizeHinter interface.
 func (msg Message) SizeHint() int {
 	return msg.VShare.SizeHint() +
@@ -111,4 +167,23 @@ func (msg *Message) Unmarshal(buf []byte, rem int) ([]byte, int, error) {
 		return buf, rem, err
 	}
 	return msg.Proof.Unmarshal(buf, rem)
+}
+
+// Generate implements the quick.Generator interface.
+func (msg Message) Generate(rand *rand.Rand, size int) reflect.Value {
+	share := shamir.VerifiableShare{
+		Share: shamir.Share{
+			Index: secp256k1.RandomFn(),
+			Value: secp256k1.RandomFn(),
+		},
+		Decommitment: secp256k1.RandomFn(),
+	}
+	com := secp256k1.RandomPoint()
+	proof := mulzkp.Proof{}.Generate(rand, size).Interface().(mulzkp.Proof)
+	m := Message{
+		VShare:     share,
+		Commitment: com,
+		Proof:      proof,
+	}
+	return reflect.ValueOf(m)
 }
